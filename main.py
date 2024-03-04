2342e072f99c0f60f088d055c5afa13a81d8760a
Daniel Salazar <dsnile.salazr@gmail.com>
2024-03-03 22:48:23 -0600

    Red Interactiva

--- main.py
+++ main.py
@@ -1,37 +1,20 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import pyvis
+from pyvis.network import Network
 
 
-g = nx.DiGraph()
-g.add_edge("O", "C", length=1)
-g.add_edge("O", "A", length=4)
-g.add_edge("A", "E", length=3)
-g.add_edge("A", "O", length=4)
-g.add_edge("A", "B", length=2)
-g.add_edge("C", "B", length=2)
-g.add_edge("C", "O", length=2)
-g.add_edge("C", "D", length=3)
-g.add_edge("B", "F", weigth=4)
-g.add_edge("B", "A", weigth=4)
-g.add_edge("B", "C", weigth=4)
-g.add_edge("B", "G", weigth=4)
-g.add_edge("D", "E", weigth=4)
-g.add_edge("D", "C", weigth=4)
-g.add_edge("E", "F", weigth=3)
-g.add_edge("E", "A", weigth=3)
-g.add_edge("E", "D", weigth=3)
-g.add_edge("F", "G", weigth=2)
-g.add_edge("F", "B", weigth=4)
-g.add_edge("F", "E", weigth=3)
-g.add_edge("G", "B", weigth=4)
-g.add_edge("G", "F", weigth=2)
-
-resultado = nx.dijkstra_path(g, source="G", target="O")
-lg = nx.dijkstra_path_length(g, source="G", target="O")
-
-print(resultado, lg)
-
-nx.draw(g, with_labels=True, font_weight='bold')
+net = Network(height="750px",font_color=True, width="100%", bgcolor="#222222", cdn_resources="remote")
 
 
-plt.show()
+
+
+net.add_nodes([1,2,3,4,5,6,7,8,9,10], label = ['David','Daniel', 'Liz','Mike','Daniel de la Fuente','Blanca','Barbara','DaniLentes','Hugo','Norma'])
+net.add_edges([(1,3,5), (2,4,10), (3,6,11), (5,4,3), (2,5,4), (2,6,6), (4,3,1), (7,2,3), (8,3,5), (1,9,10), (9,10,10),(2,3,20)])
+
+
+
+
+net.show_buttons(filter_=['physics'])
+net.show("red.html", notebook=False)
+
